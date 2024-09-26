f4657cf80cfe64d29169c3e11c437fded54e0215
Doug Johnson <doug@asknice.com>
2024-09-26 09:12:42 -0500

    add sponsors page

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -15,23 +15,23 @@
 </head>
 <body class="bg-pink-100">
 <div class="min-h-screen flex flex-col justify-center items-center">
-    <header class="text-center mb-12">
+    <header class="text-center">
         <div class="w-full max-w-4xl mx-auto mb-8 flex justify-center items-center">
             <img src="/images/logo2024-Fall3.jpg" alt="Conference Header Image" class="w-96 h-auto rounded-lg shadow-lg">
         </div>
         <h1 class="text-5xl font-extrabold text-pink-600 mb-4">Romance Writers of America Annual Conference</h1>
         <p class="text-xl text-gray-700">Join us for a weekend of inspiration, creativity, and romance!</p>
         <nav class="mx-auto my-6 max-w-container px-4 sm:px-6 lg:px-8">
-            <div class="relative flex items-center py-[2.125rem]">
+            <div class="relative flex items-center">
                 <div class="ml-auto hidden lg:flex lg:items-center">
                     <a class="bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
                     focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
                     sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/">Home</a>
 
-{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
-{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
-{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/speakers">Speakers</a>--}}
-
+                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
+                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
+                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+                       href="/sponsor">Sponsors</a>
 
                     <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
                     focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
---
+++ resources/views/pages/sponsor.blade.php
@@ -0,0 +1,65 @@
+<x-layouts.guest>
+<section>
+    <div class="bg-white" >
+        <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2>Sponsors</h2>
+
+            <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <a href="https://www.sherrilynkenyon.com/" target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200">
+                        <img src="/images/sponsors/sklogo.png" alt="Sherrilyn McQueen" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">Sherrilyn McQueen</p>
+                </a>
+                <a href="https://www.clairbrett.com/"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200">
+                        <img src="/images/sponsors/clairbrett.png" alt="Clair Brett" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Conference Sign and Pro Retreat</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">Clair Brett</p>
+                </a>
+                <a href="https://tulepublishing.com/"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200">
+                        <img src="/images/sponsors/tule.png" alt="Tule Publishing" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Totebags</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">Tule Publishing</p>
+                </a>
+                <a href="https://vellum.pub/"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200">
+                        <img src="/images/sponsors/vellum-big.png" alt="Vellum" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">Vellum</p>
+                </a>
+                <a href="https://www.lararwa.com/"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
+                        <img src="/images/sponsors/lararwa.jpg" alt="" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">Los Angeles Romance Writers</p>
+                </a>
+                <a href="https://allaboutromance.com/"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
+                        <img src="/images/sponsors/aar.jpg" alt="" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">All About Romance</p>
+                </a>
+                <a href="#"  target="_blank" class="group">
+                    <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
+                        <img src="/images/sponsors/scventures.jpg" alt="" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">WiFi</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">SC Ventures</p>
+                </a>
+
+                <!-- More products... -->
+            </div>
+        </div>
+    </div>
+
+</section>
+</x-layouts.guest>
+
