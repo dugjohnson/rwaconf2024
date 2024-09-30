71b38932174ea66d92e9697f26253296485735fa
Doug Johnson <doug@asknice.com>
2024-09-30 17:16:05 -0500

    center H2 on sponsor

--- resources/views/pages/sponsor.blade.php
+++ resources/views/pages/sponsor.blade.php
@@ -2,7 +2,9 @@
     <section>
         <div class="bg-white">
             <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2>Sponsors</h2>
+                <div class="text-center">
+                    <h2 class="text-xl font-extrabold text-pink-800 mb-4">Sponsors</h2>
+                </div>
 
                 <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     <a href="https://allaboutromance.com/" target="_blank" class="group">
