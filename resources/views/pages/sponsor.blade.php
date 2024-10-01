70af785d02f5a9284a832dc95afb6b4f8003a53d
Doug Johnson <doug@asknice.com>
2024-10-01 09:11:06 -0500

    better looking sponsor page

--- resources/views/pages/sponsor.blade.php
+++ resources/views/pages/sponsor.blade.php
@@ -6,69 +6,74 @@
                     <h2 class="text-xl font-extrabold text-pink-800 mb-4">Sponsors</h2>
                 </div>
 
-                <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 text-center">
                     <a href="https://allaboutromance.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
                             <img src="/images/sponsors/aar.jpg" alt=""
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">All About Romance</p>
                     </a>
                     <a href="https://www.clairbrett.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Conference Sign and Pro Retreat</h3>
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200">
                             <img src="/images/sponsors/clairbrett.png" alt="Clair Brett"
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Conference Sign and Pro Retreat</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">Clair Brett</p>
                     </a>
                     <a href="https://www.clairbrett.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">co-sponsor WiFi</h3>
+
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200">
                             <img src="/images/sponsors/klytics.jpg" alt="K-Lytics"
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">co-sponsor WiFi</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">K-Lytics</p>
                     </a>
                     <a href="https://www.lararwa.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
+
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
                             <img src="/images/sponsors/lararwa.jpg" alt=""
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">Los Angeles Romance Writers</p>
                     </a>
                     <a href="https://www.sherrilynkenyon.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
+
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200">
                             <img src="/images/sponsors/sklogo.png" alt="Sherrilyn McQueen"
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Badge Holder</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">Sherrilyn McQueen</p>
                     </a>
                     <a href="#" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">co-sponsor WiFi</h3>
+
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200 ">
                             <img src="/images/sponsors/scventures.jpg" alt=""
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">co-sponsor WiFi</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">SC Ventures</p>
                     </a>
                     <a href="https://tulepublishing.com/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Totebags</h3>
+
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200">
                             <img src="/images/sponsors/tule.png" alt="Tule Publishing"
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Totebags</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">Tule Publishing</p>
                     </a>
                     <a href="https://vellum.pub/" target="_blank" class="group">
+                        <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
                         <div class="w-full overflow-hidden rounded-lg bg-gray-200">
                             <img src="/images/sponsors/vellum-big.png" alt="Vellum"
                                  class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
-                        <h3 class="mt-4 text-sm text-gray-700">Bronze</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">Vellum</p>
                     </a>
                 </div>
