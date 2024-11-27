9d12122889680b2de7502759d153e21ff2eb04c2
Doug Johnson <doug@asknice.com>
2024-11-27 19:12:09 +0530

    update front page

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -17,49 +17,49 @@
 <div class="min-h-screen flex flex-col justify-center items-center">
     <header class="text-center">
         <div class="w-full max-w-4xl mx-auto mb-8 flex justify-center items-center">
-            <img src="/images/logo2024-Fall3.jpg" alt="Conference Header Image" class="w-96 h-auto rounded-lg shadow-lg">
+            <img src="/images/RWAConf2025LocationGraphic.png" alt="Conference Header Image" class="w-96 h-auto rounded-lg shadow-lg">
         </div>
         <h1 class="text-5xl font-extrabold text-pink-600 mb-4">Romance Writers of America Annual Conference</h1>
         <p class="text-xl text-gray-700">Join us for a weekend of inspiration, creativity, and romance!</p>
-        <nav class="flex items-center justify-between flex-wrap mx-auto my-6 max-w-container px-4 sm:px-6 lg:px-8">
-            <div class="relative flex items-center">
-                <div class="ml-auto hidden lg:flex lg:items-center">
-                    <a class="bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/">Home</a>
+{{--        <nav class="flex items-center justify-between flex-wrap mx-auto my-6 max-w-container px-4 sm:px-6 lg:px-8">--}}
+{{--            <div class="relative flex items-center">--}}
+{{--                <div class="ml-auto hidden lg:flex lg:items-center">--}}
+{{--                    <a class="bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/">Home</a>--}}
 
-                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
-                       href="/sponsor">Sponsors</a>
+{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"--}}
+{{--                       href="/sponsor">Sponsors</a>--}}
 
-                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
-                       href="/excursion">Excursion</a>
+{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"--}}
+{{--                       href="/excursion">Excursion</a>--}}
 
-                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/scavenger">Scavenger Hunt</a>
+{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/scavenger">Scavenger Hunt</a>--}}
 
-                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/author">Author Table Sign-up</a>
+{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/author">Author Table Sign-up</a>--}}
 
 {{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
 {{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
 {{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" href="/vendor">Vendor Sign-up</a>--}}
 {{--                    --}}
-                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2
-                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center
-                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" target="_blank"
-                       href="https://conference.rwaconference.org/e/1/romance-writers-of-america-annual-conference">Conference
-                        Registration</a>
+{{--                    <a class="ml-8 bg-pink-600 hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2--}}
+{{--                    focus:ring-offset-pink-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center--}}
+{{--                    sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" target="_blank"--}}
+{{--                       href="https://conference.rwaconference.org/e/1/romance-writers-of-america-annual-conference">Conference--}}
+{{--                        Registration</a>--}}
 
-                </div>
+{{--                </div>--}}
 
-            </div>
-        </nav>
+{{--            </div>--}}
+{{--        </nav>--}}
     </header>
     <main class="font-sans text-gray-900 antialiased">
         {{ $slot }}
@@ -67,7 +67,7 @@
 
     @livewireScripts
     <footer class="mt-12 text-gray-600">
-        <p>© 2024 Romance Writers of America. All rights reserved.</p>
+        <p>© 2024-2025 Romance Writers of America. All rights reserved.</p>
     </footer>
 </div>
 </body>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,52 +1,72 @@
 <x-layouts.guest>
     <section class="bg-white shadow-lg rounded-lg p-8 w-full max-w-4xl">
-        <h2 class="text-3xl font-bold text-pink-600 mb-4">Registration is Open!</h2>
-        <div x-data="{open:false}">
-            <p>Before you go to the registration site
-                <button class="bg-blue-300 hover:bg-blue-800focus:outline-none
+        <div class="hidden">
+            <h2 class="text-3xl font-bold text-pink-600 mb-4">Registration is Open!</h2>
+            <div x-data="{open:false}">
+                <p>Before you go to the registration site
+                    <button class="bg-blue-300 hover:bg-blue-800focus:outline-none
             text-slate font-semibold h-12 px-6 rounded-lg flex items-center justify-center
                     sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
-                        x-on:click="open = ! open">please click to read the refund policy
-                </button>
-            </p>
-            <div class="bg-slate  p-6" x-show="open">
-                <p class="text-gray-700 mb-6">Refunds requested between September 1 and September 30 shall be granted,
-                    less a $150 processing fee. No refunds after September 30.</p>
+                            x-on:click="open = ! open">please click to read the refund policy
+                    </button>
+                </p>
+                <div class="bg-slate  p-6" x-show="open">
+                    <div class="ml-12">
+                        <h3 class="text-gray-700 text-2xl mb-6 -indent-12"><strong>RWA2025 Conference Registration
+                                Refund Policy</strong></h3>
+                        <p class="text-gray-700 mb-6 -indent-12"><strong>1. Refund Deadlines and Fees:</strong><br>
+                            &bull; Refund requests received between <strong>December 3rd and February 14th</strong> will
+                            be
+                            processed with a <strong>$100 processing fee</strong>.<br>
+                            &bull; Requests received between <strong>February 15th and April 2nd</strong> will be
+                            processed
+                            with a <strong>$150 processing fee.</strong><br>
+                            &bull; Requests received between <strong>April 3rd and June 1st</strong> will be processed
+                            with a
+                            <strong>$200 processing fee</strong>.<br>
+                            &bull; <strong>No refunds</strong> will be granted after <strong>June 1st</strong>.</p><br>
+                        <p class="text-gray-700 mb-6 -indent-12"><strong>2. Payment Plan Registrations:</strong><br>
+                            &bull; The <strong>first payment is nonrefundable</strong>.</p>
+                        <p class="text-gray-700 mb-6 -indent-12"><strong>3. Exceptional Circumstances:</strong><br>
+                            Refunds may be issued without a cancellation fee if documentation is provided showing the
+                            registrant could not attend due to:<br>
+                            &bull; Serious illness or death of the registrant or an immediate family member.<br>
+                            &bull; Inability to travel due to natural disasters, war, government regulations, or acts of
+                            terrorism.</p>
+                        <p class="text-gray-700 mb-6 -indent-12"><strong>4. Comped Registration Policy:</strong>
+                            If a volunteer or committee member with a comped registration cannot attend due to serious
+                            illness, death of an immediate family member, or travel restrictions (natural disasters,
+                            war,
+                            etc.):<br>
+                            &bull; The comped registration may be applied to the following year upon submission of
+                            written
+                            proof.<br>
+                            &bull; The volunteer or committee member must pay the applicable processing fee and any
+                            increase
+                            in registration costs.<br>
+                            &bull; The comped registration is <strong>non-transferable</strong> to another individual.
+                        </p>
 
-                <p class="text-gray-700 mb-6">Refunds will be processed for registrants who provide documentation to
-                    prove
-                    they were unable to attend
-                    the Conference due to serious illness or death of registrant or the registrant's immediate family
-                    member, or registrant’s inability to travel due to natural disasters, war, government regulations,
-                    or
-                    acts of terrorism. Registrants who qualify for refunds under this provision will not be required to
-                    pay
-                    a cancellation fee.</p>
-
-                <p class="text-gray-700 mb-6">With written proof, full fees shall be refunded.</p>
-
-                <p class="text-gray-700 mb-6">If a comped registration is given to a committee member in exchange for
-                    services already rendered, and
-                    the member is unable to attend due to the member's serious illness or the death of an immediate
-                    family
-                    member, or registrant’s inability to travel due to natural disasters, war, government regulations,
-                    or
-                    acts of terrorism, then the member may use the comped fee the following year, subject to payment of
-                    the
-                    processing fee and any increase in registration fees, upon submission of written medical proof. The
-                    comped fee is not transferable to another individual.</p>
+                    </div>
+                </div>
             </div>
+            <p>You will be able to buy a ticket for the conference and indicate your preferences for meals. You will be
+                able
+                to pay for your ticket(s) in the next few days, but you can sign up for them now.
+                Your registration will not be complete until payment is received.</p>
+            <a href="https://conference.rwaconference.org/e/2/romance-writers-of-america-conference-2025"
+               class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">Proceed to the conference
+                registration area</a>
         </div>
-        <p>You will be able to buy a ticket for the conference and indicate your preferences for meals. You will be able
-            to pay for your ticket(s) in the next few days, but you can sign up for them now.
-            Your registration will not be complete until payment is received.</p>
-        <a href="https://conference.rwaconference.org/e/1/romance-writers-of-america-annual-conference"
-           class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">Proceed to the conference
-            registration area</a>
-        <p class="text-gray-700 mb-6">Get ready to immerse yourself in the world of romance writing. Stay tuned for more
-            details about speakers, workshops, and special events.</p>
-        <div>
-            <h3 class="text-xl font-bold text-pink-800 mb-4">Countdown to Inspiration</h3>
+        <div class="text-center">
+            <p class="text-gray-700 mb-6">Get ready to immerse yourself in the world of romance writing. Stay tuned for
+                more
+                details about speakers, workshops, and special events.</p>
+
+
+            <h2 class="text-3xl mt-4 font-extrabold text-pink-600 mb-4"><strong>Cyber Sale</strong> December 2 - 9 Big
+                Savings on Registration for a career changing event</h2>
+            <h3 class="text-xl font-bold text-pink-800 mb-4">Countdown to Savings</h3>
             <div id="countdown" class="countdown"></div>
         </div>
 
@@ -92,6 +112,6 @@
             const interval = setInterval(updateTimer, 1000);
         }
 
-        countdownTimer('2024-10-11T09:30:00');
+        countdownTimer('2024-12-02T09:00:00');
     </script>
 </x-layouts.guest>
