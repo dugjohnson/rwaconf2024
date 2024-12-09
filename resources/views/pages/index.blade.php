83bff3d96301583551c243fbc0bf576611daec27
Doug Johnson <doug@asknice.com>
2024-12-09 15:56:22 -0300

    update front page with pricing

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -25,27 +25,27 @@
                             <h3 class="text-2xl font-bold text-pink-600 ">Cyber Sale Registration December 2 - December
                                 8</h3>
                             <ul>
-                                <li>Member: $399 *</li>
-                                <li>Non Member:$499 *</li>
-                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $299 *</li>
+                                <li>Member: $399 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Non Member:$499 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $299 <sub class="text-2xl text-pink-600">&ast;</sub></li>
                             </ul>
                         </div>
                         <div class="mb-2">
                             <h3 class="text-2xl font-bold text-pink-600 ">Early Bird Registration December 9 - February
                                 14</h3>
                             <ul>
-                                <li>Member: $450 *</li>
-                                <li>Non Member:$550 *</li>
-                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $350 *</li>
+                                <li>Member: $450 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Non Member:$550 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $350 <sub class="text-2xl text-pink-600">&ast;</sub></li>
                             </ul>
                         </div>
                         <div class="mb-2">
                             <h3 class="text-2xl font-bold text-pink-600 ">Regular Registration February 15 - April
                                 2</h3>
                             <ul>
-                                <li>Member: $499 *</li>
-                                <li>Non Member:$599 *</li>
-                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $399 *</li>
+                                <li>Member: $499 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Non Member:$599 <sub class="text-2xl text-pink-600">&ast;</sub></li>
+                                <li>Industry Professional (Literary Agent or Publishing Company Editor):  $399 <sub class="text-2xl text-pink-600">&ast;</sub></li>
                             </ul>
                         </div>
                         <div class="mb-2">
@@ -57,7 +57,7 @@
                                 <li>Industry Professional (Literary Agent or Publishing Company Editor):  $449</li>
                             </ul>
                         </div>
-                        <div class="py-4"><p>* A payment plan is available This option includes a $26 convenience
+                        <div class="py-4"><p> <sub class="text-2xl text-pink-600">&ast;</sub> A payment plan is available This option includes a $26 convenience
                                 fee.</p></div>
 
                     </div>
